dff9e5447b816fe87f89d98e2d72d9cb871a627d
rebo95 <arebored@ucm.es>
2019-03-29 11:31:03 +0100

    shotgun behaviour, spawning bullets and shotgun view (not optimized with pooler)

---
+++ Rat Harvest/Assets/Scripts/Shooting/ShotGunBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGunBehaviour : MonoBehaviour
+{
+
+    [SerializeField]
+    private int bulletsCount;
+    [SerializeField]
+    private float bulletSpeed;
+    [SerializeField]
+    private float spreadAngele;
+
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private Transform Barrel;
+
+    List<Quaternion> bullets;
+
+
+    private void Awake()
+    {
+        bullets = new List<Quaternion>(bulletsCount);
+
+        for (int i = 0; i < bulletsCount; i++)
+        {
+            bullets.Add(Quaternion.Euler(Vector3.zero));
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            fire();
+
+    }
+
+    void fire()
+    {
+
+
+        for (int i = 0; i < bulletsCount; i++) { 
+            bullets[i] = Random.rotation;
+            GameObject bullet = Instantiate(bulletPrefab, Barrel.position, Barrel.rotation);
+            bullet.transform.rotation = Quaternion.RotateTowards(bullet.transform.rotation, bullets[i], spreadAngele);
+            bullet.GetComponent<Rigidbody>().AddForce(-bullet.transform.right * bulletSpeed);
+        }
+    }
+}
