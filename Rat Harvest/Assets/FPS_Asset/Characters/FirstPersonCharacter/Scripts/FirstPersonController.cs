1291c4896a15914383094ac2ee301a0a6526c981
rebo95 <arebored@ucm.es>
2019-04-04 13:18:48 +0200

    running logic added for controller

--- Rat Harvest/Assets/FPS_Asset/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Rat Harvest/Assets/FPS_Asset/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -239,7 +239,12 @@ namespace UnityStandardAssets.Characters.FirstPerson
 #if !MOBILE_INPUT
             // On standalone builds, walk/run speed is modified by a key press.
             // keep track of whether or not the character is walking or running
-            m_IsWalking = !Input.GetKey(KeyCode.LeftShift);
+
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetButton("Run"))
+                m_IsWalking = false;
+            else
+                m_IsWalking = true;
+            //m_IsWalking = !Input.GetKey(KeyCode.LeftShift);
 #endif
             // set the desired speed to be walking or running
             speed = m_IsWalking ? m_WalkSpeed : m_RunSpeed;
