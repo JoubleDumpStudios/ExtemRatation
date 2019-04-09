0c266d8a121fd532ee71a4dca419fc16d88f6fda
rebo95 <arebored@ucm.es>
2019-04-09 15:41:33 +0200

    looking for nearest object and avoiding obstacles implementation, just starting

--- Rat Harvest/Assets/Scripts/AI/Rat/Rat_Movement_Logic.cs
+++ Rat Harvest/Assets/Scripts/AI/Rat/Rat_Movement_Logic.cs
@@ -10,24 +10,57 @@ public class Rat_Movement_Logic : MonoBehaviour
     [SerializeField]
     Transform _destination;
 
+    
+
+    public List<GameObject> destinations;
+
     NavMeshAgent _navMeshAgent;
+
+    public bool lookForNewTarget = false;
+
+    
     // Start is called before the first frame update
     void Start()
     {
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
-
-        setDestination();
+        
+        setDestination(destinations[calculateNearestObjectIndex()]);
     }
 
-    private void setDestination()
+    private void setDestination(GameObject obstacle)
     {
-        Vector3 targetVector = _destination.transform.position;
-        _navMeshAgent.SetDestination(targetVector);
+        //Vector3 targetVector = _destination.transform.position;
+        _navMeshAgent.SetDestination(obstacle.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (lookForNewTarget)
+        {
+            setDestination(destinations[calculateNearestObjectIndex()]);
+            lookForNewTarget = false;
+        }
+    }
+
+    int calculateNearestObjectIndex()
+    {
+        int indicator = 0;
+        float distance = 0;
+        float nearestDistance = 2000;
+
+        for (int i = 0; i<destinations.Count; i++)
+        {
+            if (destinations[i] != null) {
+                distance = Vector3.Distance(this.transform.position, destinations[i].transform.position);
+
+                if (nearestDistance > distance)
+                {
+                    nearestDistance = distance;
+                    indicator = i;
+                }
+            }
+        }
+        return indicator;
     }
 }
