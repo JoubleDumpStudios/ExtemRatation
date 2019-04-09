084d262e9ca50aea51c1085e668bdf705131fed1
rebo95 <arebored@ucm.es>
2019-04-09 12:31:34 +0200

    nav mesh starting point

---
+++ Rat Harvest/Assets/Scripts/AI/Rat/Rat_Movement_Logic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Rat_Movement_Logic : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        setDestination();
+    }
+
+    private void setDestination()
+    {
+        Vector3 targetVector = _destination.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
