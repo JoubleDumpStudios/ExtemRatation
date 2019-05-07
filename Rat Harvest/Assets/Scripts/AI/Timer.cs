96f6be310b240649f5967b6982b38cc8dd56b22c
rebo95 <arebored@ucm.es>
2019-05-07 14:38:45 +0200

    timer and new canvas functionalities

--- Rat Harvest/Assets/Scripts/AI/Timer.cs
+++ Rat Harvest/Assets/Scripts/AI/Timer.cs
@@ -1,40 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField]
+    Text preRoundText;
+
+    [SerializeField]
     private List<Spawner> spawnerList;
 
-    private bool startRound;
+    [SerializeField]
+    private int startRoundTime;
+    float time = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        preRoundText.text = "Round Starts In:";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void updateSpawner()
-    {
-        for (int i = 0; i < spawnerList.Count; i++)
-            spawnerList[i].StartRound = true;
-    }
-
-    void updatePlant(Spawner spawner)
-    {
-        int i = 0;
 
 
-        if (spawner.PatchOfSoil.PlantPoints[i].HasCrop)
+
+        time += Time.deltaTime;
+
+        if (time >= startRoundTime)
         {
-            //spawner.PatchOfSoil.PlantStandPoints.Plant
+            TurnOnSpawnerAndPlantGrowing();
         }
 
+        if (time <= startRoundTime)
+            preRoundText.text = "Round Starts In " + startRoundTime + " seconds : " + time.ToString("F2");
+        else
+            preRoundText.text =" Rats are awake !!!";
+    }
+
+    void TurnOnSpawnerAndPlantGrowing()
+    {
+        for (int i = 0; i < spawnerList.Count; i++)
+        {
+            spawnerList[i].StartRound = true;
+            UpdatePlant(spawnerList[i]);
+        }
+    }
+
+    void UpdatePlant(Spawner spawner)
+    {
+        List<PlantPoint> plantPoints = spawner.PatchOfSoil.PlantPoints;
+
+        for (int i = 0; i < plantPoints.Count; i++)
+        {
+            PlantPoint plantPoint = plantPoints[i];
+            if (plantPoint.HasCrop)
+            {
+                plantPoint.Plant.GetComponent<Plant_Behaviour>().StartRound = true;
+            }
+        }
     }
 }
