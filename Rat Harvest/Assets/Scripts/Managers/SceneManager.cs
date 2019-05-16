972c5f841fbe094b4fd09ade24e1f6c80590b879
rebo95 <arebored@ucm.es>
2019-05-16 17:13:01 +0200

    Game transitions main menu-credits done, transition menu game done, buttons are working and coloring when the mose is over them, the credits are implemented and working

---
+++ Rat Harvest/Assets/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadCredits()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Credits");
+    }
+
+    public void loadGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Test_LevelDesign_Jon");
+    }
+
+    public void loadMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
