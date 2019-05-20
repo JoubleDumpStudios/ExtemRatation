0bc9f68c6c7efc88818fcfa926134ec321cabf04
rebo95 <arebored@ucm.es>
2019-05-20 23:54:46 +0200

    timer setting improved

--- Rat Harvest/Assets/Scripts/AI/Timer.cs
+++ Rat Harvest/Assets/Scripts/AI/Timer.cs
@@ -13,6 +13,8 @@ public class Timer : MonoBehaviour
 
     [SerializeField]
     private int startRoundTime;
+    private string minsAndSecsstartRoundTime_String;
+
     float time = 0;
 
     [SerializeField]
@@ -22,15 +24,27 @@ public class Timer : MonoBehaviour
     [SerializeField]
     private float harvestingTime;
 
+    private string minsAndSecsTime_String;
+    private string minsAndSecsHarvestingTime_String;
+
     bool resetTimer = false;
     bool preRound = true;
+
     // Start is called before the first frame update
     void Start()
     {
         preRoundText.text = "Round Starts In:";
+
+        MinsAndSecondsConverter(ref minsAndSecsHarvestingTime_String, harvestingTime);
+        MinsAndSecondsConverter(ref minsAndSecsstartRoundTime_String, startRoundTime);
     }
 
-
+    void MinsAndSecondsConverter(ref string outPutString, float timeToChange )
+    {
+        int mins = Mathf.FloorToInt(timeToChange / 60F);
+        int secs = Mathf.FloorToInt(timeToChange - mins * 60);
+        outPutString = string.Format("{0:00}:{1:00}", mins, secs);
+    }
 
     // Update is called once per frame
     void Update()
@@ -43,10 +57,24 @@ public class Timer : MonoBehaviour
             TurnOnSpawnerAndPlantGrowing();
         }
 
+        MinsAndSecondsConverter(ref minsAndSecsTime_String, time);
+
+        PreroundLogicAndText();
+
+
+        if (resetTimer)
+        {
+            time = 0;
+            resetTimer = false;
+        }
+    }
+
+    void PreroundLogicAndText()
+    {
         if (preRound)
         {
             if (time <= startRoundTime)
-                preRoundText.text = "Round Starts In " + startRoundTime + " seconds : " + time.ToString("F2");
+                preRoundText.text = "Round Starts In " + minsAndSecsstartRoundTime_String + "  -> " + minsAndSecsTime_String/*time.ToString("F2")*/;
             else if (time <= startRoundTime + ratsAreAwakeTextTime)
                 preRoundText.text = " Rats are awake !!!";
             else
@@ -56,19 +84,13 @@ public class Timer : MonoBehaviour
                 resetTimer = true;
             }
         }
-        else if(time <= harvestingTime /*+ startRoundTime + ratsAreAwakeTextTime*/)
+        else if (time <= harvestingTime /*+ startRoundTime + ratsAreAwakeTextTime*/)
         {
-            preRoundText.text = "You have " + harvestingTime + " seconds to harvest how much you can " + time.ToString("F2");
-        }else if (time > harvestingTime)
+            preRoundText.text = "You have " + minsAndSecsHarvestingTime_String + " seconds to harvest how much you can " + minsAndSecsTime_String /*time.ToString("F2")*/;
+        }
+        else if (time > harvestingTime)
         {
             preRoundText.text = "Round Over";
-        }
-
-
-        if (resetTimer)
-        {
-            time = 0;
-            resetTimer = false;
         }
     }
 
