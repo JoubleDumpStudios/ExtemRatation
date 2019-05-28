23167aebbcf50d7805a476df5bd23254aed6404f
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2019-05-28 14:14:13 +0200

    Updated rat shatter prefab with automatic explosion

---
+++ Rat Harvest/Assets/Test/Rat_Explosion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rat_Explosion : MonoBehaviour
+{
+
+    public float radius = 5.0F;
+    public float power = 200.0F;
+    public float upwardsModifier = 0.2f;
+    public Transform explosionPoint;
+
+    void Start()
+    {
+        Debug.Log("bang");
+        Vector3 explosionPos = explosionPoint.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, upwardsModifier);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
